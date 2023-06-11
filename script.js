59b556f0951af8fc02036b1a456cd9496bdbd83b
Manikanta <pillimetlamanikanta@gmail.com>
2023-06-11 21:56:53 +0530

    color

---
+++ script.js
@@ -0,0 +1,20 @@
+let mainContainerElement = document.getElementById("mainContainer");
+let hexCodeElement = document.getElementById("hexCode");
+
+
+function onChangeGreen() {
+    mainContainerElement.style.backgroundColor = "Green",
+    hexCodeElement.textContent ="Green"
+}
+function onChangeYellow() {
+    mainContainerElement.style.backgroundColor = "Yellow",
+    hexCodeElement.textContent ="Yellow"
+}
+function onChangeViolet() {
+    mainContainerElement.style.backgroundColor = "Violet",
+    hexCodeElement.textContent ="Violet"
+}
+function onChangeAqua() {
+    mainContainerElement.style.backgroundColor = "Aqua",
+    hexCodeElement.textContent ="Aqua"
+}
